735ee4861351e1619fc925917db520d30c535622
shivamrai9 <Shivamrai5476@gmail.com>
2024-11-18 21:29:56 +0530

    Initial commit

---
+++ src/features/product-list/productListAPI.js
@@ -0,0 +1,9 @@
+export function fetchCount() {
+    return new Promise(async (resolve) =>{
+      const response = await fetch('http://localhost:8080')
+      const data = await response.json();
+      resolve({data})
+    }
+    );
+  }
+  
